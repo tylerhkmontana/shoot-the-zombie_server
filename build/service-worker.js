f919bb50435faa35204b8297dd88a95b10332137
Hyunbin Kim <Hyunbin>
2020-11-17 03:23:23 -0500

    fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c5a379c799b05e7cdac4ff564c7995b.js"
+  "/precache-manifest.3232f87619df42b66fd10257a02d0d90.js"
 );
 
 self.addEventListener('message', (event) => {
